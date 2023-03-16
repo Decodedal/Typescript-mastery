08fb724b57a9a3ca038e2a7c2864cf62d9dfb321
Dallas Palumbo <palumbodallas@gmail.com>
2023-03-15 20:13:47 -0400

    first lines

---
+++ basics.js
@@ -0,0 +1,4 @@
+console.log("hello");
+console.log("goodbye");
+console.log(Math.round(4.33));
+var pi = 3.1415;
---
+++ basics.ts
@@ -0,0 +1,4 @@
+console.log("hello")
+console.log("goodbye")
+console.log(Math.round(4.33))
+let pi : number = 3.1415
