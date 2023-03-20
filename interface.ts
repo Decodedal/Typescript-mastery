a790ed545de79e511eaf723abc65071679fd6d38
Dallas Palumbo <palumbodallas@gmail.com>
2023-03-20 11:27:28 -0400

    types v interface

--- interface.ts
+++ interface.ts
@@ -55,3 +55,81 @@ const shirt : Product = {
 
 console.log(shirt.price)
 console.log(shirt.applyDiscount(20))
+
+//declaring an interface twice results in the interface being equal to the sum of the two definitions
+//aka they both are used to define the type;
+
+interface Dog{
+    breed : string;
+    bark(): string;
+}
+
+interface Dog{
+    name:string;
+    age: number;
+}
+
+const elton: Dog = {
+    name: "Elton",
+    age:0.5,
+    breed: "mutt",
+    bark(){
+        return "Bark"
+    }
+};
+
+//we also get extendes with interfaces just like with classes 
+
+interface ServiceDog extends Dog{
+    job:"drug sniffer" | "bomb disarming" | "Guide"
+}
+
+const Buddy : ServiceDog ={
+    name:"Bubs",
+    age:99,
+    breed:"muddy",
+    bark(){return "bark"},
+    job: "Guide"
+}
+
+//interfaces can extend more then one interface conjunctivly 
+
+interface Human{
+    name: string;
+};
+
+interface Employee{
+    readonly id :number;
+    email: string;
+};
+
+interface Engineer extends Human, Employee{
+    level: string;
+    skills: string[];
+};
+
+const Dally : Engineer = {
+    name:"Dallas",
+    id:1,
+    email:"soemthing@gamil.com",
+    level:"Master",
+    skills: ["js", "Tc", "react"]
+};
+
+console.log(Dally)
+
+//so what are the diffrences between types and interfaces 
+//interfaces only describe the shape of an object that is it!
+//type is more flexable and can be used when not working with objects
+
+type PrimaryColor = "red" | "blue" | "green"
+
+//types can not be reopened like interfaces can 
+
+type cat ={
+    name : string
+}
+
+type cat = {
+    age: number
+}
